b69f424ae6e249279a5d937b087691e010eea913
Katarzyna Da Silva <conic916@gmail.com>
2019-03-18 14:59:07 +0100

    add carousel and js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-var elem = document.querySelector('.main-carousel');
+var elem = document.querySelector('.carousel');
 var flkty = new Flickity( elem, {
   // options
-  cellAlign: 'left',
-  contain: true
+ 	cellAlign: 'left',
+ 	contain: true,
+  	prevNextButtons: false,
+	pageDots: false,
+	hash: true,
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var nextButton = document.querySelector('.button-next');
+nextButton.addEventListener( 'click', function() {
+  flkty.next();
+});
+
+var previousButton = document.querySelector('.button-previous');
+previousButton.addEventListener( 'click', function() {
+  flkty.previous();
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
