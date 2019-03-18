4b62beae924db5f99ef2b7d57768c058db1f77dd
Katarzyna Da Silva <conic916@gmail.com>
2019-03-19 00:51:31 +0100

    add file for vendor and images jpg

--- js/script.js
+++ js/script.js
@@ -26,3 +26,18 @@ flkty.on('scroll', function (progress) {
 });
 
 
+// mustache :
+/************* MUSTACHE pętla *****************/
+var listItems = '';
+var templateItem = document.getElementById('template-product-item').innerHTML;
+//Mustache.parse(templateItem);
+
+for(var i = 0; i < carouselData.length; i++){
+		//console.log(carouselData[i]);
+		listItems += Mustache.render(templateItem, carouselData[i]);
+	};
+
+//var fullList = Mustache.render(listItems);
+
+results.insertAdjacentHTML('beforeend', listItems);
+
