acd0a6082e18bf4400da516482c6735fd83976c6
Katarzyna Da Silva <conic916@gmail.com>
2019-03-18 16:00:20 +0100

    add scss

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
-  // options
- 	cellAlign: 'left',
- 	contain: true,
-  	prevNextButtons: false,
-	pageDots: false,
-	hash: true,
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false,
+    hash: true,
 });
 
 var nextButton = document.querySelector('.button-next');
-nextButton.addEventListener( 'click', function() {
-  flkty.next();
+nextButton.addEventListener('click', function () {
+    flkty.next();
 });
 
 var previousButton = document.querySelector('.button-previous');
-previousButton.addEventListener( 'click', function() {
-  flkty.previous();
+previousButton.addEventListener('click', function () {
+    flkty.previous();
 });
 
 var progressBar = document.querySelector('.progress-bar')
 
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
