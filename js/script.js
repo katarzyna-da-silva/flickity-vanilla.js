b60cfbaec8714faab98a2f51cbba5dc3ef2bae3c
Katarzyna Da Silva <conic916@gmail.com>
2019-03-20 15:11:04 +0100

    add index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+
+'use strict';
+(function(){ 
+
+	// Definujemy funkcję initMap w zakresie globalnym (czyli jako właściwość obiektu window).
+  	window.initMap = function() {
+		
+		// Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+		var uluru = {lat: -25.363, lng: 131.044};
+		
+		// W zmiennej map zapisujemy nową instancję obiektu Map. 
+		var map = new google.maps.Map(document.getElementById('map'), {
+			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+			zoom: 4,
+			center: uluru
+		});
+		
+		// Definiujemy marker jako nową instancję obiektu Marker.
+		var marker = new google.maps.Marker({
+			// I podajemy opcje tego markera, np. na której mapie ma być dodany oraz jakie są jego współrzędne. 
+			position: uluru,
+			map: map
+		}); 
+	}	
+
+initMap();
+
+})();  
+
 //MUSTACHE pętla
 
 var listItems = '';
